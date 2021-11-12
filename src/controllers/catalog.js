7017c994857e122ce28640a15cd459cf882eeeec
Erick Silva <erickrsilva@hotmail.com>
2021-11-12 07:48:57 -0300

    Route products created

---
+++ src/controllers/catalog.js
@@ -0,0 +1,24 @@
+import connection from "../database.js";
+
+const getProducts = async(req, res) => {
+    
+    try{
+        const result = await connection.query(`
+            SELECT
+                "public.products".id,
+                "public.products".name,
+                "public.products".description,
+                "public.products".price,
+                "public.products".cover,
+                "public.products".banner
+            FROM "public.products";
+        `);
+
+        res.send(result.rows);
+    }
+    catch{
+        res.sendStatus(500);
+    }
+}
+
+export { getProducts }
