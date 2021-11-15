df36132e85dd215339866c07452bc56a6f3746cf
Erick Silva <erickrsilva@hotmail.com>
2021-11-15 10:58:48 -0300

    Purchase register route done

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import { postPurchase } from './controllers/payment.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.post("/purchases", postPurchase);
 
 export default app;
